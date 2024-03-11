9997ea602c1fdd4f8176d5cb80b2407dff9064b0
Parv Sharma <sharmaparv.2004@gmail.com>
2024-03-11 15:36:24 +0530

    dynamics bar done

--- ProgressBar/v1(animated)/script.js
+++ ProgressBar/v1(animated)/script.js
@@ -1,21 +1,21 @@
-function updateProgressBar(value) {
-    const progressBar = document.getElementById('progress-bar');
-    const progress = document.getElementById('progress-container');
-    progressBar.style.width = value + '%';
-    if (value < 30) {
-      progressBar.style.backgroundColor = "red";
-      progress.style.borderColor = "red";
-    } else if (value >= 30 && value < 60) {
-      progressBar.style.backgroundColor = "orange";
-      progress.style.borderColor = "orange";
-    } else if (value >= 60 && value <= 80) {
-      progressBar.style.backgroundColor = "#3d7ae3";
-      progress.style.borderColor = "#3d7ae3";
-    } else if (value >= 80 && value <= 90) {
-      progressBar.style.backgroundColor = "blue";
-      progress.style.borderColor = "blue";
-    } else {
-      progressBar.style.backgroundColor = "green";
-      progress.style.borderColor = "green";
-    }
+function updateProgressBarDynamic(value) {
+  const progressBarDynamic = document.getElementById('progress-bar-dynamic');
+  const progressDynamic = document.getElementById('progress-container-dynamic');
+  progressBarDynamic.style.width = value + '%';
+  if (value < 30) {
+    progressBarDynamic.style.backgroundColor = "red";
+    progressDynamic.style.borderColor = "red";
+  } else if (value >= 30 && value < 60) {
+    progressBarDynamic.style.backgroundColor = "orange";
+    progressDynamic.style.borderColor = "orange";
+  } else if (value >= 60 && value <= 80) {
+    progressBarDynamic.style.backgroundColor = "#3d7ae3";
+    progressDynamic.style.borderColor = "#3d7ae3";
+  } else if (value >= 80 && value <= 90) {
+    progressBarDynamic.style.backgroundColor = "blue";
+    progressDynamic.style.borderColor = "blue";
+  } else {
+    progressBarDynamic.style.backgroundColor = "green";
+    progressDynamic.style.borderColor = "green";
   }
+}
